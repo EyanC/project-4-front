174170734327f51a6e78774c6d11c7204d676ebd
Jaime Castro <jenri@MacBook-Pro.lan>
2022-12-23 00:01:58 -0500

    front add

--- django_dogs_front/src/App.js
+++ django_dogs_front/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import Add from './components/Add';
 
-function App() {
+const App = () => {
+
+  let [dog, setDog] = useState([])
+
+  const getDog = () => {
+    axios
+      .get('http://localhost:8000/api/dog')
+      .then(
+        (response) => setDog(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error))
+  }
+
+  const handleCreate = (addPet) => {
+    axios
+      .post('http://localhost:8000/api/dog', addPet)
+      .then((response) => {
+        console.log(response)
+        getDog()
+      })
+  }
+  
+
+  useEffect(() => {
+    getDog()
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Add handleCreate={handleCreate} />
+      <div className="dog">
+        {dog.map((pet) => {
+          return (
+            <div className="pet" key={pet.id}>
+              <h4>Name: {pet.image_url}</h4>
+              <h5>Age: {pet.breed}</h5>
+              <h5>Age: {pet.age}</h5>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
+
