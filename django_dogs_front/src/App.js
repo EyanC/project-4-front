e5d9511cafc707dc03c48bbca7367606ca77bd7d
Eyan Cea <eyan.moses@gmail.com>
2022-12-27 15:35:11 -0800

    Working Code and Routes

--- django_dogs_front/src/App.js
+++ django_dogs_front/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Add from './components/Add';
+import Edit from './components/Edit';
 
 const App = () => {
 
@@ -24,6 +25,24 @@ const App = () => {
         getDog()
       })
   }
+
+  const handleDelete = (event) => {
+    axios
+      .delete('http://localhost:8000/api/dog/' + event.target.value)
+      .then((response) => {
+        getDog()
+      })
+  }
+  
+  const handleUpdate = (editDog) => {
+    console.log(editDog)
+    axios
+      .put('http://localhost:8000/api/dog/' + editDog.id, editDog)
+      .then((response) => {
+        getDog()
+      })
+  }
+  
   
 
   useEffect(() => {
@@ -34,14 +53,26 @@ const App = () => {
 
   return (
     <>
+    <header>Show Dogs</header>
+    <div className=' m-3 bg-light d-flex text-center container-fluid'>
     <Add handleCreate={handleCreate} />
-      <div className="dog">
+    </div>
+      <div className="dog container-fluid w+100">
         {dog.map((pet) => {
           return (
-            <div className="pet" key={pet.id}>
-              <h4>Name: {pet.image_url}</h4>
-              <h5>Age: {pet.breed}</h5>
+            <div className="pet bg-light card text-center m-300" key={pet.id}>
+              <img src= {pet.image_url}/>
+              <h4>Name: {pet.name}</h4>
+              <h5>Breed: {pet.breed}</h5>
               <h5>Age: {pet.age}</h5>
+              <h5>Gender: {pet.gender}</h5>
+              <h5>Color: {pet.color}</h5>
+              <h5>Size: {pet.size}</h5>
+              <h5>Description: {pet.description}</h5>
+              <button><Edit handleUpdate={handleUpdate} id={pet.id} pet={pet} /></button>
+              <button onClick={handleDelete} value={pet.id}>
+              Delete
+             </button>
             </div>
           )
         })}
