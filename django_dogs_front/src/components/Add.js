682b8727f772e667b1aa863a298fac323879507e
Eyan Cea <eyan.moses@gmail.com>
2022-12-27 11:51:15 -0800

    general update

--- django_dogs_front/src/components/Add.js
+++ django_dogs_front/src/components/Add.js
@@ -29,37 +29,37 @@ const Add = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="breed">Breed: </label>
-        <input type="text" name="breed" placeholder='what breed is your dog' value={pet.breed} onChange={handleChange}/>
+        <input type="text" name="breed" placeholder='what breed is your dog' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="gender">Gender: </label>
-        <input type="text" name="gender" placeholder='add gender' value={pet.gender} onChange={handleChange}/>
+        <input type="text" name="gender" placeholder='add gender' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="color">Color: </label>
-        <input type="text" name="color" placeholder='what color is your dog' value={pet.color} onChange={handleChange}/>
+        <input type="text" name="color" placeholder='what color is your dog' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="size">Size: </label>
-        <input type="text" name="size" placeholder='what size is your dog' value={pet.size} onChange={handleChange}/>
+        <input type="text" name="size" placeholder='what size is your dog' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="age">Age: </label>
-        <input type="number" name="age" placeholder='how old is your pet' value={pet.age} onChange={handleChange}/>
+        <input type="number" name="age" placeholder='how old is your pet' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="description">Description: </label>
-        <input type="text" name="description" placeholder='please describre your doggo' value={pet.description} onChange={handleChange}/>
+        <input type="text" name="description" placeholder='please describre your doggo' onChange={handleChange}/>
         
         <br />
         <br />
         <label htmlFor="image_url">Image: </label>
-        <input type="text" name="image" placeholder='we want pictures' value={pet.image_url} onChange={handleChange}/>
+        <input type="text" name="image" placeholder='we want pictures' onChange={handleChange}/>
         <br />
         <br />
         <input type="submit"/>
