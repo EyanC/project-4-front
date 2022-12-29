9cfd360728622da26cedad5cedfe90bda8ab39e2
Jaime Castro <jenri@MacBook-Pro.lan>
2022-12-28 20:54:13 -0500

    br

--- django_dogs_front/src/App.js
+++ django_dogs_front/src/App.js
@@ -27,49 +27,22 @@ const App = () => {
   }
 
   const handleDelete = (event) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9e74a78 (working)
-    axios.delete('http://localhost:8000/api/dog/' + event.target.value)
-    .then((res) => {
-      console.log(res.data)
-      getDog()
-    })
-<<<<<<< HEAD
-  }
-
-  const handleUpdate = (editPet) => {
-    axios.put('http://localhost:8000/api/dog/' + editPet.id, editPet)
-    .then((res) => {
-      console.log(editPet)
-      getDog()
-    })
-  }
-
-=======
     axios
       .delete('http://localhost:8000/api/dog/' + event.target.value)
       .then((response) => {
         getDog()
       })
-=======
->>>>>>> 9e74a78 (working)
   }
-
-  const handleUpdate = (editPet) => {
-    axios.put('http://localhost:8000/api/dog/' + editPet.id, editPet)
-    .then((res) => {
-      console.log(editPet)
-      getDog()
-    })
-  }
-<<<<<<< HEAD
   
->>>>>>> e5d9511 (Working Code and Routes)
-=======
-
->>>>>>> 9e74a78 (working)
+  const handleUpdate = (editDog) => {
+    console.log(editDog)
+    axios
+      .put('http://localhost:8000/api/dog/' + editDog.id, editDog)
+      .then((response) => {
+        getDog()
+      })
+  }
+  
   
 
   useEffect(() => {
@@ -87,26 +60,11 @@ const App = () => {
       <div className="dog container-fluid w+100">
         {dog.map((pet) => {
           return (
-<<<<<<< HEAD
-            <div className="pet" key={pet.id}>
-              <h4>Name: {pet.image_url}</h4>
-              <h5>Name: {pet.name}</h5>
-              <h5>Breed: {pet.breed}</h5>
-              <h5>Age: {pet.age}</h5>
-              <Edit handleUpdate={handleUpdate} id={pet.id} pet={pet} />
-              <button onClick={handleDelete} value={pet.id}>
-                X
-              </button>
-=======
             <div className="pet bg-light card text-center m-300" key={pet.id}>
               <img src= {pet.image_url}/>
               <h4>Name: {pet.name}</h4>
               <h5>Breed: {pet.breed}</h5>
               <h5>Age: {pet.age}</h5>
-              <Edit handleUpdate={handleUpdate} id={pet.id} pet={pet} />
-              <button onClick={handleDelete} value={pet.id}>
-                X
-              </button>
               <h5>Gender: {pet.gender}</h5>
               <h5>Color: {pet.color}</h5>
               <h5>Size: {pet.size}</h5>
@@ -115,7 +73,6 @@ const App = () => {
               <button onClick={handleDelete} value={pet.id}>
               Delete
              </button>
->>>>>>> e5d9511 (Working Code and Routes)
             </div>
           )
         })}
@@ -125,4 +82,3 @@ const App = () => {
 }
 
 export default App
-
