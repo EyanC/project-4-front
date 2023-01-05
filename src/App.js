866b0f3cf913d9001627660949bbbd68562a8635
levonal <lwshabee@gmail.com>
2023-01-04 18:50:19 -0600

    deploying

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import { confirmAlert } from 'react-confirm-alert';
 import Dog from './components/Dog';
 import Navbar from './components/Navbar';
 import { ColorRing } from 'react-loader-spinner';
-
-
 const App = () => {
-
   let [dog, setDog] = useState([])
   const [isFetchingDog, setIsFetchingDog] = useState(false);
   const [filteredDog, setFilteredDog] = useState([])
   const [isSearching, setIsSearching] = useState(false);
   const [currentUser, setCurrentUser] = useState({})
-
   const getDog = () => {
     setIsFetchingDog(true);
     axios
@@ -30,7 +26,6 @@ const App = () => {
         setIsFetchingDog(false);
       })
   };
-
   const handleCreate = (addPet) => {
     axios
       .post('https://morning-harbor-71338.herokuapp.com/api/dog', addPet)
@@ -39,15 +34,27 @@ const App = () => {
         getDog()
       })
   }
-
-  const handleDelete = (event) => {
-    axios
-      .delete('https://morning-harbor-71338.herokuapp.com/api/dog/' + event.target.value)
-      .then((response) => {
-        getDog()
-      })
+  const handleDelete = (deleteDog) => {
+    confirmAlert({
+      title: 'Confirm Deletion',
+      message: `Are you sure you want to delete this post?`,
+      buttons: [{
+        label: 'Yes',
+        onClick: () => {
+          axios
+          .delete('https://morning-harbor-71338.herokuapp.com/api/dog/' + deleteDog.id)
+            .then((response) => {
+              setDog(dog.filter(dog => dog.id !== deleteDog.id))
+            })
+        }
+      },
+      {
+        label: 'No',
+        onClick: () => { }
+      }
+      ]
+    })
   }
-  
   const handleUpdate = (editDog) => {
     console.log(editDog)
     axios
@@ -56,17 +63,16 @@ const App = () => {
         getDog()
       })
   }
-  
   const onSearchChange = (searchInput) => {
     const searchInputLower = searchInput.toLowerCase()
     if (searchInput.length > 0) {
       setIsSearching(true)
       const result = dog.filter((pet) => {
-        return pet.name.toLowerCase().match(searchInputLower) || 
-        pet.breed.toLowerCase().match(searchInputLower) || 
-        pet.size.toLowerCase().match(searchInputLower) || 
-        pet.gender.toLowerCase().match(searchInputLower) || 
-        pet.description.toLowerCase().match(searchInputLower) || 
+        return pet.name.toLowerCase().match(searchInputLower) ||
+        pet.breed.toLowerCase().match(searchInputLower) ||
+        pet.size.toLowerCase().match(searchInputLower) ||
+        pet.gender.toLowerCase().match(searchInputLower) ||
+        pet.description.toLowerCase().match(searchInputLower) ||
         pet.color.toLowerCase().match(searchInputLower)
       })
       setFilteredDog(result);
@@ -74,44 +80,45 @@ const App = () => {
       setIsSearching(false)
     }
   }
-
   const NoSearchResults = () => {
     return (
       <><p className="noResults">No Pets to Display</p></>
     )
   }
-
   const dogToDisplay = isSearching ? filteredDog : dog
-
-
-
-
   useEffect(() => {
     getDog()
   }, [])
-
-
-
   return (
     <>
-  
   <Navbar handleCreate={handleCreate} onSearchChange={onSearchChange} currentUser={currentUser} setCurrentUser={setCurrentUser}  />
     <div className=' m-2 bg-light d-flex text-center container-fluid w-25 '>
     {/* <Add handleCreate={handleCreate} /> */}
     </div>
-      <div className=" dog container-fluid w-100 row ">
-        {dog.map((pet) => {
-          return (
-       
-        })}
+      <div className='posts-container'>
+        {
+          isFetchingDog ? <div className='spinner'>
+            <>
+              <ColorRing
+                visible={true}
+                height='200'
+                width='200'
+                ariaLabel='blocks-loading'
+                wrapperStyle={{}}
+                wrapperClass='blocks-wrapper'
+                colors={['#C444B9', '#9E2419', '#C444B9', '#9E2419', '#C444B9']} />
+            </>
+          </div> :
+            dogToDisplay.length > 0 ?
+              dogToDisplay.map((pet) => {
+                return (
+                  <Dog pet={pet} handleUpdate={handleUpdate} handleDelete={handleDelete} key={pet.id} />
+                )
+              })
+              : <NoSearchResults />
+        }
       </div>
-
-      
-
-
     </>
   )
 }
-
 export default App
-
